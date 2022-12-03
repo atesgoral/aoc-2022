178eda64044fd73c4c0b9fa4e86df07727bc5f5b
Ates Goral <ates.goral@shopify.com>
2022-12-03 15:27:23 -0500

    Add template

---
+++ template/test.js
@@ -0,0 +1,30 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
+import {heredoc, loadInput} from '../utils.js';
+
+const part1 = (i) =>
+i.length
+
+const part2 = (i) =>
+i.length*2
+
+test('dev', () => {
+  const input = heredoc`
+    ABC
+  `;
+  const output1 = part1(input);
+  const output2 = part2(input);
+
+  assert.strictEqual(output1, 4);
+  assert.strictEqual(output2, 8);
+});
+
+test('official', () => {
+  const input = loadInput(import.meta);
+  const output1 = part1(input);
+  const output2 = part2(input);
+
+  assert.strictEqual(output1, 5);
+  assert.strictEqual(output2, 10);
+});
