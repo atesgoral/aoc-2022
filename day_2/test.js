14b8484ee00ffcd9f854861bb4982ebfe637d9ee
Ates Goral <ates.goral@shopify.com>
2022-12-03 12:59:03 -0500

    Day 2 part 2

--- day_2/test.js
+++ day_2/test.js
@@ -3,8 +3,10 @@ import assert from 'node:assert/strict';
 
 import {heredoc, loadInput} from '../utils.js';
 
-const solution = (i) =>
+const part1 = (i) =>
 i.split`\n`.map(p=>p.split` `.map(c=>parseInt(c,36)%10)).map(([t,u])=>(u-t+4)%3*3+u-2|0).reduce((s,n)=>s+n)
+const part2 = (i) =>
+i.split`\n`.map(p=>p.split` `.map(c=>parseInt(c,36)%10)).map(([t,o])=>(o-3)*3+(o+t+2)%3+1|0).reduce((s,n)=>s+n)
 
 test('dev', () => {
   const input = heredoc`
@@ -12,15 +14,19 @@ test('dev', () => {
     B X
     C Z
   `;
-  const output = solution(input);
+  const output1 = part1(input);
+  const output2 = part2(input);
 
-  assert.strictEqual(output, 15);
+  assert.strictEqual(output1, 15);
+  assert.strictEqual(output2, 12);
 });
 
 test('official', () => {
   const input = loadInput(import.meta);
-  const output = solution(input);
+  const output1 = part1(input);
+  const output2 = part2(input);
 
-  assert.strictEqual(output, 12535);
+  assert.strictEqual(output1, 12535);
+  assert.strictEqual(output2, 15457);
 });
 
