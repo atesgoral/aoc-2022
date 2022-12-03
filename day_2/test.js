7821475b2ef12c80a149d31ea2abe8d98a8a008b
Ates Goral <ates.goral@shopify.com>
2022-12-03 15:26:58 -0500

    Add line

--- day_2/test.js
+++ day_2/test.js
@@ -5,6 +5,7 @@ import {heredoc, loadInput} from '../utils.js';
 
 const part1 = (i) =>
 i.split`\n`.map(p=>p.split` `.map(c=>parseInt(c,36)%10)).map(([t,u])=>(u-t+4)%3*3+u-2|0).reduce((s,n)=>s+n)
+
 const part2 = (i) =>
 i.split`\n`.map(p=>p.split` `.map(c=>parseInt(c,36)%10)).map(([t,o])=>(o-3)*3+(o+t+2)%3+1|0).reduce((s,n)=>s+n)
 
