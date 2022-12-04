9730cbd178d28e089d7e70f89e39bcf3ed51a3ea
Ates Goral <ates.goral@shopify.com>
2022-12-03 23:20:02 -0500

    Day 3 part 1

--- day_3/test.js
+++ day_3/test.js
@@ -3,8 +3,11 @@ import assert from 'node:assert/strict';
 
 import {heredoc, loadInput} from '../utils.js';
 
+// Predefine vars to overcome strict mode
+let b, l, m;
+
 const part1 = (i) =>
-157
+i.split`\n`.map(k=>Math.log2(Number((m=(k,c)=>[...k.substring(b=(l=k.length/2)*c,b+l)].map(c=>c.charCodeAt()^64).map(c=>(c^32)-(c>>5^1)*6-1).reduce((n,b)=>n|1n<<BigInt(b),0n))(k,0)&m(k,1)))+1|0).reduce((a,b)=>a+b)
 
 const part2 = (i) =>
 i.length*2
@@ -30,6 +33,6 @@ test('official', () => {
   const output1 = part1(input);
   const output2 = part2(input);
 
-  assert.strictEqual(output1, 5);
+  assert.strictEqual(output1, 7568);
   assert.strictEqual(output2, 10);
 });
