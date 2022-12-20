c1a9c9a24d8cdcbbadfee6695eac433b0358bbb9
Ates Goral <ates.goral@shopify.com>
2022-12-20 14:47:55 +0100

    Day 3 abandoned

--- day_3/test.js
+++ day_3/test.js
@@ -7,10 +7,33 @@ import {heredoc, loadInput} from '../utils.js';
 let b, l, m;
 
 const part1 = (i) =>
+// i.split`\n`.map(
+//   k=>Math.log2(
+//     Number(
+//       (
+//         m=(k,c)=>[...k.substring(b=(l=k.length/2)*c,b+l)]
+//           .map(c=>c.charCodeAt()^64)
+//           .map(c=>(c^32)-(c>>5^1)*6-1)
+//           .reduce((n,b)=>n|1n<<BigInt(b),0n)
+//       )(k,0)
+//       &
+//       m(k,1)))+1|0).reduce((a,b)=>a+b)
 i.split`\n`.map(k=>Math.log2(Number((m=(k,c)=>[...k.substring(b=(l=k.length/2)*c,b+l)].map(c=>c.charCodeAt()^64).map(c=>(c^32)-(c>>5^1)*6-1).reduce((n,b)=>n|1n<<BigInt(b),0n))(k,0)&m(k,1)))+1|0).reduce((a,b)=>a+b)
 
 const part2 = (i) =>
-i.length*2
+i.split`\n`.map(
+  k=>[...k]
+    .map(c=>c.charCodeAt()^64)
+    .map(c=>(c^32)-(c>>5^1)*6-1)
+    .reduce((n,b)=>n|1n<<BigInt(b),0n)
+).reduce((a,k,i)=>{
+  if (!i%3) {
+    a.s+=Math.log2(Number(a.b)+1)
+    a.b=0n
+  }
+  a.b&=k
+  return a
+},{s:0,b:0n})
 
 test('dev', () => {
   const input = heredoc`
